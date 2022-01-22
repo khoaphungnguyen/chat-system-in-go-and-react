bea27065c0429debb6aadefbee4b4cd657a844dd
Rex Phung <khoatuanphungnguyen@gmail.com>
2022-01-22 15:50:40 -0800

    Design Frontend to look like a decent online chat application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect, sendMsg} from  './api';
+import Header from './components/Header';
+import ChatHistory from './components/ChatHistory';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    connect()
+    this.state = {
+      chatHistory: []
+    }
   }
 
   send(){
@@ -12,9 +16,21 @@ class App extends Component {
     sendMsg("hello")
   }
 
+  componentDidMount(){
+    connect((msg)=>{
+      console.log("New Message")
+      this.setState(prevState=>({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state)
+    });
+  };
+
   render(){
     return (
       <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory}/>
         <button onClick={this.send}>Hit</button>
       </div>
     )
