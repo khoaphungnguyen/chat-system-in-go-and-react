e3807c185b41bb253165f73c83e0504266652656
Rex Phung <khoatuanphungnguyen@gmail.com>
2022-01-22 22:44:09 -0800

    Improve Backend to handle multiple clients and boardcast messages to everyone connected to the pool

--- backend/main.go
+++ backend/main.go
@@ -2,62 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/khoaphungnguyen/chat-system-in-go-and-react/pkg/websocket"
 )
 
-// Define an upgrader wih a Read and Write buffer size
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	// Need to check the original of the connection
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// Define a reader which listens for new message to the websocket endpoint
-func reader(conn *websocket.Conn) {
-	for {
-		// read message
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		// print the message
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-// Define the websocket endpoint
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	// Upgrade this connection to websocket connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-	// listen infinite messages go through the websocket
-	reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
-	// map our '/ws' endpoint to the 'serveWs' function
-	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-	fmt.Println("Chat App V0.01")
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
